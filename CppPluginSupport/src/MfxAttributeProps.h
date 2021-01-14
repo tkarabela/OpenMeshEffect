a1aef0934ab452843449203caab28e0472f4fd0d
Tomas Karabela <tkarabela@seznam.cz>
2021-01-14 23:35:38 +0100

    Add value getter/setter to MfxAttributeProps

--- CppPluginSupport/src/MfxAttributeProps.h
+++ CppPluginSupport/src/MfxAttributeProps.h
@@ -21,4 +21,48 @@ struct MfxAttributeProps
     int componentCount;
     char* data;
     bool isOwner;
+
+    /**
+     * Get value at particular index.
+     */
+    bool GetValue(int *value, int i, int component = 0) {
+        switch (type) {
+            case MfxAttributeType::UByte: *value = ((unsigned char*)(data + i*stride))[component]; return true;
+            case MfxAttributeType::Int: *value = ((int*)(data + i*stride))[component]; return true;
+            case MfxAttributeType::Float: break;
+            case MfxAttributeType::Unknown: break;
+        }
+        return false;
+    }
+    bool GetValue(float *value, int i, int component = 0) {
+        switch (type) {
+            case MfxAttributeType::UByte: *value = ((unsigned char*)(data + i*stride))[component]; return true;
+            case MfxAttributeType::Int: *value = ((int*)(data + i*stride))[component]; return true;
+            case MfxAttributeType::Float: *value = ((float*)(data + i*stride))[component]; return true;
+            case MfxAttributeType::Unknown: break;
+        }
+        return false;
+    }
+
+    /**
+     * Set value at particular index.
+     */
+    bool SetValue(int value, int i, int component = 0) {
+        switch (type) {
+            case MfxAttributeType::UByte: ((unsigned char*)(data + i*stride))[component] = value; return true;
+            case MfxAttributeType::Int: ((int*)(data + i*stride))[component] = value; return true;
+            case MfxAttributeType::Float: ((float*)(data + i*stride))[component] = value; return true;
+            case MfxAttributeType::Unknown: break;
+        }
+        return false;
+    }
+    bool SetValue(float value, int i, int component = 0) {
+        switch (type) {
+            case MfxAttributeType::UByte: break;
+            case MfxAttributeType::Int: break;
+            case MfxAttributeType::Float: ((float*)(data + i*stride))[component] = value; return true;
+            case MfxAttributeType::Unknown: break;
+        }
+        return false;
+    }
 };
